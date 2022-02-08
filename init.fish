dfdf2287af4fa4c0f529131057026bc16b282790
Daniel Altiparmak <daniel.altiparmak@tomcom.de>
2022-02-08 21:37:25 +0100

    initial commit

---
+++ init.fish
@@ -0,0 +1,7 @@
+# fishssh initialization hook
+#
+# You can use the following variables in this file:
+# * $package       package name
+# * $path          package path
+# * $dependencies  package dependencies
+ssh_agent_init
